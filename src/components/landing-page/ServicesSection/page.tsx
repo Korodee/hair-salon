ec549aaea733275afd36f00f0f7b141daa6e2b15
Korode <koroskki@gmail.com>
2025-02-06 15:26:10 +0100

    Dashboard First style done

--- src/components/landing-page/ServicesSection/page.tsx
+++ src/components/landing-page/ServicesSection/page.tsx
@@ -12,13 +12,13 @@ const servicesData = [
       "Some example text is placed here to show what this component will look like fully built out.",
   },
   {
-    icon: "/img/service1.jpg",
+    icon: "/img/service2.jpg",
     title: "Haircuts & Styling",
     description:
       "Some example text is placed here to show what this component will look like fully built out.",
   },
   {
-    icon: "/img/service1.jpg",
+    icon: "/img/service3.jpg",
     title: "Massage & Spa",
     description:
       "Some example text is placed here to show what this component will look like fully built out.",
